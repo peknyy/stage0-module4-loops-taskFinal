e2d840c4ec443eedb69f8d67e1138a52db444db3
peknyy <lukcymr3@gmail.com>
2023-09-13 13:39:51 +0600

    Done

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
-        if(numberTableToPrint <= 0 ) System.out.println("Please, prompt positive integer");
+        if(numberTableToPrint < 0 ) System.out.println("Please, prompt positive integer");
         else{
             for (int i = 1; i <= 10; i++) {
                 System.out.println(i + " x " + numberTableToPrint +" = "+ i * numberTableToPrint);
